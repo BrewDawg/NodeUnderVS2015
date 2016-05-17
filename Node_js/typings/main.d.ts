d77c74b99a140b6073062d0c35dae3c022e80e4d
mike.griffin <mike.griffin@brewdawg.com>
2016-05-17 11:06:31 -0400

    Working on typings

--- Node_js/typings/main.d.ts
+++ Node_js/typings/main.d.ts
@@ -1,4 +1,3 @@
-
 /// <reference path="main/ambient/body-parser/index.d.ts" />
 /// <reference path="main/ambient/express-serve-static-core/index.d.ts" />
 /// <reference path="main/ambient/express/index.d.ts" />
